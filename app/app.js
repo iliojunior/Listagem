17840dae0905a1643b1c883b146e15156631e6cb
Ilio Junior <junior@ingadigital.com.br>
2017-03-28 17:31:42 -0300

    Generating folders...

--- app/app.js
+++ app/app.js
@@ -6,9 +6,11 @@
 
     app.controller("PrincipalController", ['$scope', '$http', PrincipalController]);
 
-    document.querySelector("#search").focus();
+    app.controller("CreateController", ['$scope', '$http', CreateController]);
+
 
     function PrincipalController($scope, $http) {
+        document.querySelector("#search").focus();
         $scope.directories = [];
 
         $http.get("getDir.php")
@@ -20,6 +22,48 @@
             .catch(function (err) {
                 console.error("Erro: " + err);
             });
+    }
+
+    function CreateController($scope, $http) {
+        $scope.subpastas = [];
+        $scope.nova_pasta = "";
+        $scope.nova_subpasta = "";
+
+        function adicionar_subpasta($nova_subpasta) {
+            if ($scope.nova_pasta === undefined || $scope.nova_pasta === "") {
+                alert("Informe o nome da pasta!");
+                document.querySelector("#nova_pasta").focus();
+            } else {
+                $scope.subpastas.push($nova_subpasta);
+                $scope.nova_subpasta = "";
+            }
+        }
+
+        function criar_nova_pasta() {
+
+            $http.post("createDir.php",
+                {
+                    nome_pasta: $scope.nova_pasta,
+                    sub_pastas: $scope.subpastas
+                })
+                .then(function (response) {
+                    alert("Salvo com sucesso! \r\n" + response.data);
+                    resetNovos();
+                })
+                .catch(function (response) {
+                    alert("Erro ao salvar! \r\n" + response);
+                });
+        }
+
+        function resetNovos() {
+            $scope.nova_pasta = "";
+            $scope.nova_subpasta = "";
+            $scope.subpastas = [];
+        }
+
+        $scope.adicionar_subpasta = adicionar_subpasta;
+        $scope.criar_nova_pasta = criar_nova_pasta;
+
     }
 
 })();
---
+++ createDir.php
@@ -0,0 +1,31 @@
+<?php
+
+function isValidJSON($str)
+{
+    json_decode($str);
+    return json_last_error() == JSON_ERROR_NONE;
+}
+
+$json_params = file_get_contents("php://input");
+
+if (strlen($json_params) > 0 && isValidJSON($json_params))
+    $decoded_params = json_decode($json_params);
+
+$pathPasta = "../" . $decoded_params->nome_pasta;
+
+
+if (!is_dir($pathPasta)) {
+    mkdir($pathPasta);
+    echo "Pasta '{$decoded_params->nome_pasta}' criada!\r\n";
+} else
+    echo "Pasta já existe: {$pathPasta}!\r\n";
+
+foreach ($decoded_params->sub_pastas as $sub_pasta) {
+    $path_subPasta = $pathPasta . "/" . $sub_pasta;
+
+    if (!is_dir($path_subPasta)) {
+        mkdir($path_subPasta);
+        echo "Sub pasta '{$sub_pasta}' criada!\r\n";
+    } else
+        echo "Sub pasta já existe: {$path_subPasta}!\r\n";
+}
